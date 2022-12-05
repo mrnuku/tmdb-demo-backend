817c0fc7f935dbf6d35a49fd5883c973d7edbe1d
mrnuku <mrnuku@gmail.com>
2022-12-05 13:48:21 +0100

    add detail endpoint

--- index.ts
+++ index.ts
@@ -17,13 +17,27 @@ server.get<SearchRequest>('/search', async (request, reply) => {
   .then(res => res.data)
 })
 
+interface DetailRequest extends RequestGenericInterface {
+  Querystring: {
+    id: number
+  }
+}
+
+server.get<DetailRequest>('/detail', async (request, reply) => {
+  return axios.get(`https://api.themoviedb.org/3/movie/${request.query.id}`, {
+    headers: { Accept: 'application/json', 'Accept-Encoding': 'identity' }, // workaround for gzip encoding bug
+    params: {api_key: process.env.TMDB_API_KEY}
+  })
+  .then(res => res.data)
+})
+
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
   if (process.env.TMDB_API_KEY === undefined) {
-    console.error("TMDB_API_KEY environment variable missing");
+    console.error("TMDB_API_KEY (v3) environment variable missing");
     process.exit(1);
   }
   console.log(`Server listening at ${address}`);
