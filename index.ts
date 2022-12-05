6accefc21d20e1be2bdc19ce502f70b85869930e
mrnuku <mrnuku@gmail.com>
2022-12-05 11:03:58 +0100

    add search endpoint

--- index.ts
+++ index.ts
@@ -1,15 +1,30 @@
-import fastify from 'fastify'
+import fastify, { RequestGenericInterface } from 'fastify';
+import axios from "axios";
 
-const server = fastify()
+const server = fastify();
 
-server.get('/ping', async (request, reply) => {
-  return 'pong\n'
+interface SearchRequest extends RequestGenericInterface {
+  Querystring: {
+    q: string
+  }
+}
+
+server.get<SearchRequest>('/search', async (request, reply) => {
+  return axios.get("https://api.themoviedb.org/3/search/movie", {
+    headers: { Accept: 'application/json', 'Accept-Encoding': 'identity' }, // workaround for gzip encoding bug
+    params: {api_key: process.env.TMDB_API_KEY, query: request.query.q}
+  })
+  .then(res => res.data)
 })
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-  console.log(`Server listening at ${address}`)
-})
+  if (process.env.TMDB_API_KEY === undefined) {
+    console.error("TMDB_API_KEY environment variable missing");
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
